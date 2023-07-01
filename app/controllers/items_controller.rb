4fed6dbd8be760c354f4bef6a6a00c74b650e5c6
masanaga225150 <vvma67310@gmail.com>
2023-07-01 14:30:30 +0900

    不要なコメントアウトを削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,16 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :redirect_to_root_if_not_owner, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
-  # arthenticate_user!は、Deviseが提供するメソッドで、ログイン状態のチェックを行います。
-  # ユーザーがログインしていなければ、そのユーザーをログイン画面に遷移させる。
-  # before_actionで呼び出すことで、アクションを実行する前にログインしていなければログイン画面に遷移させられる。
 
   def index
-    @items = Item.all.order(created_at: :desc) # 複数データになるので、@itemsにする
-    # 商品の一覧を取得する
+    @items = Item.all.order(created_at: :desc) 
   end
 
   def new
-    @item = Item.new # newメソッド Itemクラスの新しいインスタンスを作成
+    @item = Item.new 
   end
 
-  def create # createアクションを定義
+  def create 
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
@@ -48,8 +44,7 @@ class ItemsController < ApplicationController
 
 
   def item_params
-    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :shipping_fee_id, :delivery_prefecture_id, :shipping_duration_id, :price).merge(user_id: current_user.id) # item_paramsというストロングパラメーターを定義し、createメソッドの引数に使用して、itemsテーブルへ保存できるようにした。
-    # 画像の保存を許可するストロングパラメーター
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :shipping_fee_id, :delivery_prefecture_id, :shipping_duration_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
