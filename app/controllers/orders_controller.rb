de55cc22342180086442c14857fa9d9e0e8b59e1
masanaga225150 <vvma67310@gmail.com>
2023-07-06 17:04:17 +0900

    商品売却済み時の画面遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
      @item = Item.find(params[:item_id])
-    if current_user == @item.user
+    if current_user == @item.user || @item.order.present?
        redirect_to root_path
     else
      @form_order = FormOrder.new
